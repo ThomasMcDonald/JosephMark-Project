e0eea5d4a6e9822660776c7c1a85aef7393d2aca
Thomas McDonald <tommcd1996@gmail.com>
2018-06-28 02:32:21 +0000

    Build

--- src/utils/ticket-api.js
+++ src/utils/ticket-api.js
@@ -3,22 +3,25 @@ const baseURl = 'https://jm-ticketing.herokuapp.com'
 
 export { getTickets, addTicket, deleteTicket, resolveTicket }
 
+// Request all tickets
 function getTickets () {
   const url = `${baseURl}/api/tickets`
   return axios.get(url).then(response => response.data.tickets)
 }
 
+// Request to add ticket
 function addTicket (params) {
   const url = `${baseURl}/api/addticket`
   return axios.post(url, { params: params })
 }
 
-// Delete Ticket, probably not necessary but I did it
+// Request to delete Ticket, probably not necessary but I did it
 function deleteTicket (id) {
   const url = `${baseURl}/api/deleteticket/` + id
   return axios.post(url)
 }
 
+// Request to resolve the given ticket.
 function resolveTicket (id) {
   const url = `${baseURl}/api/resolveTicket`
   return axios.post(url, { params: id })
--- src/utils/user-Api.js
+++ src/utils/user-Api.js
@@ -4,11 +4,13 @@ const baseURl = 'https://jm-ticketing.herokuapp.com'
 
 export { validateCredentials, isActiveToken, getUsers, registerUser }
 
+// Validate credentials with server
 function validateCredentials (params) {
   const url = `${baseURl}/api/user/authenticate`
   return axios.post(url, { params: params })
 }
 
+// verify that the current user token is still active
 function isActiveToken () {
   const url = `${baseURl}/api/user/verifyToken`
   return localforage.getItem('token').then((jwt) => {
@@ -16,11 +18,13 @@ function isActiveToken () {
   })
 }
 
+// Request all users
 function getUsers () {
   const url = `${baseURl}/api/user/getUsers`
   return axios.get(url).then(response => response.data.users)
 }
 
+// Request to add user
 function registerUser (params) {
   const url = `${baseURl}/api/user/register`
   return axios.post(url, { params: params })
